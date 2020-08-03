6466614c1d23ebba46f4d7946729cfeac782d27b
Tomohiro Hagino <marspeoplehg@gmail.com>
2020-08-03 13:17:24 +0900

    01a_rubocopの自動修正 && usersテーブルのインデックス設定

--- app/services/jwt/token_decripter.rb
+++ app/services/jwt/token_decripter.rb
@@ -1,18 +1,17 @@
 module Jwt::TokenDecryptor
   extend self
- 
+
   def call(token)
     decrypt(token)
   end
- 
+
   private
+
   def decrypt(token)
-    begin
-      JWT.decode(token, Rails.application.secrets.secret_key_base)
-    rescue 
-       raise InvalidTokenError
-    end
+    JWT.decode(token, Rails.application.secrets.secret_key_base)
+  rescue StandardError
+    raise InvalidTokenError
   end
 end
- 
-class InvalidTokenError < StandardError; end;
+
+class InvalidTokenError < StandardError; end
