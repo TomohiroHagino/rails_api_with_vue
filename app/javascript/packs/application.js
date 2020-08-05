9d3663b0cd6fb42ac496219b4351d3c239bc1f22
Tomohiro Hagino <marspeoplehg@gmail.com>
2020-08-05 17:33:52 +0900

    02_ユーザー登録/ログイン認証（フロントエンド）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,36 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+import Vue from 'vue'
+import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
+import router from '@/router'
+import store from '@/store'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        vuetify,
+        router,
+        store,
+        created() {
+            const userString = localStorage.getItem('currentUser')
+            if (userString) {
+                const userData = JSON.parse(userString)
+                this.$store.commit('auth/SET_CURRENT_USER', userData)
+            }
+        },
+        render: h => h(App)
+    }).$mount()
+    document.body.appendChild(app.$el)
+
+    console.log(app)
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
+// RailsにWebpackerでVue.jsを入れたら行う設定
+// https://qiita.com/kazutosato/items/c0e508f15b9a03f3dd6b
 const { environment } = require('@rails/webpacker')
 
+// bin/webpack-dev-serverビルド時にエラーが出る。
+// vue-loader v15 から VueLoaderPlugin を読み込むようになったのでこの設定が必要になった。
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const vue =  require('./loaders/vue')
+const aliasConfig = require('./alias')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+environment.config.merge(aliasConfig)
 module.exports = environment
